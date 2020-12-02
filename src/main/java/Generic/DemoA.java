30eb5d665c0839912b4d27870e015cd6add9dd4e
user <user@user-PC>
2020-12-02 11:46:59 +0530

    Abc

--- src/main/java/Generic/DemoA.java
+++ src/main/java/Generic/DemoA.java
@@ -1,6 +1,15 @@
 package Generic;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class DemoA 
 {
+	@Test
+	public void tc_01()
+	{
+		Reporter.log("Github",true);
+	}
+	
 
 }
